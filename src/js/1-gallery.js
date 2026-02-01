20ced59e4cd89fda9dac79cc27293cae70a508d9
Dmytro Drapak <d.dstudy05@gmail.com>
2026-02-01 15:00:59 +0200

    test1

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -3,48 +3,48 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
   {
-    preview: '../img/gallery/1-1.jpg',
-    original: '../img/gallery/1-1large.jpg',
+    preview: './img/gallery/1-1.jpg',
+    original: './img/gallery/1-1large.jpg',
     description: 'Design Research',
   },
   {
-    preview: '../img/gallery/1-2.jpg',
-    original: '../img/gallery/1-2large.jpg',
+    preview: './img/gallery/1-2.jpg',
+    original: './img/gallery/1-2large.jpg',
     description: 'Hand with Lightbulb',
   },
   {
-    preview: '../img/gallery/1-3.jpg',
-    original: '../img/gallery/1-3large.jpg',
+    preview: './img/gallery/1-3.jpg',
+    original: './img/gallery/1-3large.jpg',
     description: 'Mobile Development',
   },
   {
-    preview: '../img/gallery/2-1.jpg',
-    original: '../img/gallery/2-1large.jpg',
+    preview: './img/gallery/2-1.jpg',
+    original: './img/gallery/2-1large.jpg',
     description: 'Modern Office',
   },
   {
-    preview: '../img/gallery/2-2.jpg',
-    original: '../img/gallery/2-2large.jpg',
+    preview: './img/gallery/2-2.jpg',
+    original: './img/gallery/2-2large.jpg',
     description: 'Meeting Room',
   },
   {
-    preview: '../img/gallery/2-3.jpg',
-    original: '../img/gallery/2-3large.jpg',
+    preview: './img/gallery/2-3.jpg',
+    original: './img/gallery/2-3large.jpg',
     description: 'Coworking Space',
   },
   {
-    preview: '../img/gallery/3-1.jpg',
-    original: '../img/gallery/3-1large.jpg',
+    preview: './img/gallery/3-1.jpg',
+    original: './img/gallery/3-1large.jpg',
     description: 'Workspace with Coffee',
   },
   {
-    preview: '../img/gallery/3-2.jpg',
-    original: '../img/gallery/3-2large.jpg',
+    preview: './img/gallery/3-2.jpg',
+    original: './img/gallery/3-2large.jpg',
     description: 'Working Process',
   },
   {
-    preview: '../img/gallery/3-3.jpg',
-    original: '../img/gallery/3-3large.jpg',
+    preview: './img/gallery/3-3.jpg',
+    original: './img/gallery/3-3large.jpg',
     description: 'Business Presentation',
   },
 ];
